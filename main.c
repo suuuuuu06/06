be24973062809b05d75d43ed9db425431b0052b0
suuuuuu06 <bsj06@sookmyung.ac.kr>
2022-10-11 17:33:37 +0900

    5

--- main.c
+++ main.c
@@ -3,14 +3,30 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
-{
-	return(a*a);
+
+
+int combination(int n, int r)
+{	
+	return (factorial(n)/(factorial(n-r)*factorial(r)));
 }
 
-int main()
+int factorial(int x)
 {
-	int a=2;
-	a=square(a);
-	printf("a=%i\n",a);
+	int i,res=1;
+	for (i=1;i<=x;i++)
+		res=res*i;
+	return res;
 }
+
+int main(void)
+{
+	int n,r,result;
+	printf("두 가지 정수를 입력하시오: ");
+	scanf("%d %d",&n,&r);
+	
+	result=combination(n,r);
+	printf("%dC%d=%d",n,r,result);
+	
+}
+
+
